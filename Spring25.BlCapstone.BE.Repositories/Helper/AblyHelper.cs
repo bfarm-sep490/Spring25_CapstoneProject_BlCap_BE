47632c36c5ee2eb2dc8e06a57ce2ba11e1d25ece
thangbinhbeo <binhbeopro1122@gmail.com>
2025-01-13 22:03:25 +0700

    feat: set up ably

---
+++ Spring25.BlCapstone.BE.Repositories/Helper/AblyHelper.cs
@@ -0,0 +1,65 @@
+﻿using FirebaseAdmin.Messaging;
+using IO.Ably;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spring25.BlCapstone.BE.Repositories.Helper
+{
+    public class AblyHelper
+    {
+        private readonly AblyRest _ablyClient;
+        private readonly string _channelName = "notifications";
+
+        public AblyHelper()
+        {
+            var ablyConfig = Environment.GetEnvironmentVariable("ABLY_API_KEY");
+            if (string.IsNullOrEmpty(ablyConfig))
+            {
+                throw new Exception("Ably configuration is missing...");
+            }
+
+            _ablyClient = new AblyRest(ablyConfig);
+        }
+
+        public async Task<string> SendNotificationAsync(string title, string body)
+        {
+            try
+            {
+                var channel = _ablyClient.Channels.Get(_channelName);
+                await channel.PublishAsync("notification", new
+                {
+                    title = title,
+                    body = body
+                });
+
+                return "Push notifications successfully!";
+            } 
+            catch (AblyException ex)
+            {
+                return $"Push notifications failed: {ex.Message}";
+            }
+        }
+
+        public async Task<string> SendMessageWithTopic(string title, string body, string topic)
+        {
+            try
+            {
+                var channel = _ablyClient.Channels.Get(topic);
+                await channel.PublishAsync(topic, new
+                {
+                    title = title,
+                    body = body
+                });
+
+                return "Push notifications successfully!";
+            }
+            catch (AblyException ex)
+            {
+                return $"Push notifications failed: {ex.Message}";
+            }
+        }
+    }
+}
