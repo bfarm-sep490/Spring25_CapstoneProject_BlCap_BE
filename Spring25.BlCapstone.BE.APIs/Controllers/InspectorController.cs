2a09dc0fff7e5268624568dfbaab90ab3b8001b4
thangbinhbeo <binhbeopro1122@gmail.com>
2025-03-19 22:21:09 +0700

    feat: notication for all roles

--- Spring25.BlCapstone.BE.APIs/Controllers/InspectorController.cs
+++ Spring25.BlCapstone.BE.APIs/Controllers/InspectorController.cs
@@ -2,6 +2,7 @@
 using Spring25.BlCapstone.BE.Services.BusinessModels.Farmer;
 using Spring25.BlCapstone.BE.Services.BusinessModels.Inspector;
 using Spring25.BlCapstone.BE.Services.Services;
+using System.ComponentModel.DataAnnotations;
 
 namespace Spring25.BlCapstone.BE.APIs.Controllers
 {
@@ -76,6 +77,48 @@ namespace Spring25.BlCapstone.BE.APIs.Controllers
         {
             var rs = await _inspectorService.UploadImage(image);
             return Ok(rs);
+        }
+
+        [HttpPost("{id}/device-token")]
+        public async Task<IActionResult> AddTokenDevice(int id, [Required] string tokenDevice)
+        {
+            try
+            {
+                var rs = await _inspectorService.AddInspectorTokenDevice(id, tokenDevice);
+                return Ok(rs);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("{id}/device-token")]
+        public async Task<IActionResult> GetListTokens(int id)
+        {
+            try
+            {
+                var rs = await _inspectorService.GetAllDeviceTokensByInspectorId(id);
+                return Ok(rs);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}/device-token")]
+        public async Task<IActionResult> RemoveToken(int id)
+        {
+            try
+            {
+                var rs = await _inspectorService.RemoveDeviceTokenByInspectorId(id);
+                return Ok(rs);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"{ex.Message}");
+            }
         }
     }
 }
